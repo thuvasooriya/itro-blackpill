f62f5f211c0a167658d193be04a2a2328e630d6c
thuvasooriya <thuvaragan28@gmail.com>
2023-11-29 11:09:52 +0530

    small change

--- src/main.cpp
+++ src/main.cpp
@@ -181,21 +181,18 @@ void signal_1()
 
 void pwm_test()
 {
-  int8_t x = 3;
+  int8_t x = 1;
+  int8_t k = 3;
   digitalWrite(AIN1, HIGH);
   digitalWrite(AIN2, LOW);
   digitalWrite(BIN1, HIGH);
   digitalWrite(BIN2, LOW);
-  for (int i = 0; i <= 255; i++)
-  {
+  for (int i = 0; i > -1; i = i + x) {
     analogWrite(PWMA, i);
     analogWrite(PWMB, i);
-    delay(x*10);
-  }
-  for (int i = 255; i >= 0; i--)
-  {
-    analogWrite(PWMA, i);
-    analogWrite(PWMB, i);
-    delay(x*10);
+    if (i == 255) {
+      x = -1;  // switch direction at peak
+    }
+    delay(k*10);
   }
 }
